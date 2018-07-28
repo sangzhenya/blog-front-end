c09feeee6703793543798d997fb7369b6d0fa7c3
sangzhenya <sangzhenya@gmail.com>
2018-07-29 01:36:13 +0800

    rechange to web

--- src/config/common-config.js
+++ src/config/common-config.js
@@ -1,5 +1,6 @@
 let config = {};
 
-config.webDomain = 'http://localhost:8080/';
+// config.webDomain = 'http://localhost:8080/';
+config.webDomain = 'https://web.sangzhenya.com/';
 
 export default config;
