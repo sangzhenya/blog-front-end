299bcd3afc401e37af54332502a8e4f629c8ff83
sangzhenya <sangzhenya@gmail.com>
2019-08-04 21:02:40 +0800

    Re Init

--- src/config/common-config.js
+++
@@ -1,6 +0,0 @@
-let config = {};
-
-// config.webDomain = 'http://localhost:8080/';
-config.webDomain = 'https://web.sangzhenya.com/';
-
-export default config;
--- src/main.js
+++ src/main.js
@@ -1,23 +1,12 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
-import iView from 'iview';
-// import 'iview/dist/styles/iview.css';
+import store from './store'
 
-if (process.env.NODE_ENV === 'development') {
-  require('iview/dist/styles/iview.css')
-}
+Vue.config.productionTip = false
 
-Vue.config.productionTip = false;
-
-Vue.use(iView);
-
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-});
+  store,
+  render: h => h(App)
+}).$mount('#app')
